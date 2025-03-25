054ee0348cfda7a97f60cda24df290935189f11c
davidblackwell121 <david.blackwell121@gmail.com>
2025-03-24 18:34:19 -0600

    Payment API ID added in comments

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,3 @@
-// Test for Git commit
+// Placeholders until proper inplementation please don't delete and please keep safe
+// Client ID: ARjrMdSPjUfimQVcSAWFcUXAxuSJw3zaOjAZS5hljEqJySrkTUIUnRppWpB2S6aHsOMahstq9XuCTYpY
+// Client secret: EFZORs7KzKjEZBTF-0TkW6Qcu8_0NQeBSaU0aI_LuniN0xVdGYsK8Kb8m98hV0W7_26YT69j5NElK7t6
