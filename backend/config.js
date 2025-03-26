7933e46f3b8eda861d2f6d85cfe65bb9d6b6e756
davidblackwell121 <david.blackwell121@gmail.com>
2025-03-26 17:15:09 -0600

    Added a simple notification system with reminders, moved files into components and css folders

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
 
 module.exports = {
-  stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY, // Used to access Stripe Payment Processing
 };
